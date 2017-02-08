f1946b2aee3d3eab96fb5aa13e7e8f7f116fa9a2
Peter Krautzberger <p.krautzberger@gmail.com>
2017-02-08 10:25:23 +0100

    [main.js] add missing checks for htmlNode Fixes #288

---
+++ test/issue288.js
@@ -0,0 +1,26 @@
+var tape = require('tape');
+var mjAPI = require("../lib/main.js");
+
+tape('HTML output when requesting both SVG and HTML', function(t) {
+    t.plan(2);
+    mjAPI.start();
+    var tex = 'x';
+
+    mjAPI.typeset({
+        math: tex,
+        format: "TeX",
+        htmlNode: true,
+        svgNode: true
+    }, function(data) {
+        t.equal(data.htmlNode.className, 'mjx-chtml MJXc-display', 'htmlNode output is correct');
+    });
+
+    mjAPI.typeset({
+        math: tex,
+        format: "TeX",
+        html: true,
+        svgNode: true
+    }, function(data) {
+        t.ok(data.html, 'html output is present')
+    });
+});
