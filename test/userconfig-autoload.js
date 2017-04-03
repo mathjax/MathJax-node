8f18918725c652654983422c1cbd9c1f82645834
Peter Krautzberger <p.krautzberger@gmail.com>
2017-04-03 17:34:19 +0200

    Fix #307 (#314)
    * [test] add test for #307
    
    * [main] user configuration with autload-all should enable color extension
    Fixes #307

---
+++ test/userconfig-autoload.js
@@ -0,0 +1,20 @@
+var tape = require('tape');
+var mjAPI = require("../lib/main.js");
+
+tape('User config: autoload-all should enable color extension', function(t) {
+    t.plan(1);
+
+    var tex = '\\definecolor{myorange}{RGB}{255,165,100}\\color{myorange}e^{i \\pi}\\color{Black} = -1';
+    mjAPI.config( {
+        extensions: 'TeX/autoload-all', // a convenience option to add MathJax extensions
+    });
+    mjAPI.start();
+
+    mjAPI.typeset({
+        math: tex,
+        format: "inline-TeX",
+        mml: true
+    }, function(data) {
+        t.ok(!data.errors, 'definecolor should be a known function');
+    });
+});
