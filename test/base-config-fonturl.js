7e6481813ff0c2c6952f29123f48044876c7ea78
Peter Krautzberger <p.krautzberger@gmail.com>
2017-05-02 14:41:50 +0200

    [test] add test for fontURL configuration

---
+++ test/base-config-fonturl.js
@@ -0,0 +1,31 @@
+var tape = require('tape');
+var mjAPI = require("../lib/main.js");
+
+tape('basic configuration: check fontURL', function (t) {
+    t.plan(2);
+
+    var tex = 'a';
+    mjAPI.typeset({
+        math: tex,
+        format: "TeX",
+        css: true
+    }, function (result, data) {
+        t.ok(result.css.indexOf('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/fonts/HTML-CSS') > -1, 'Default fontURL');
+    });
+    // reconfigure
+    mjAPI.typeset({
+        math: ''
+    }, function(){
+        mjAPI.config({
+            fontURL: 'https://example.com'
+        });
+        mjAPI.start();
+    })
+    mjAPI.typeset({
+        math: tex,
+        format: "TeX",
+        css: true,
+    }, function (result, data) {
+        t.ok(result.css.indexOf('https://example.com') > -1, 'Configuring fontURL');
+    });
+});
