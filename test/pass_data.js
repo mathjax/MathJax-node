60c2c8dca68a381e9be160fd7276cf0eb21ac45c
Peter Krautzberger <p.krautzberger@gmail.com>
2017-05-02 22:10:15 +0200

    [main] pass input data to output Resolves #303

---
+++ test/pass_data.js
@@ -0,0 +1,16 @@
+var tape = require('tape');
+var mjAPI = require("../lib/main.js");
+
+tape('Passing data along', function(t) {
+    t.plan(1);
+    mjAPI.start();
+    var tex = 'x';
+    mjAPI.typeset({
+        math: tex,
+        format: "TeX",
+        css: true,
+        something: 'expected',
+    }, function(data, input) {
+        t.equal(input.something, 'expected', 'Data was passed along to output');
+    });
+});
